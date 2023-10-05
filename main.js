d3af439f4c84aa5930c83af146432d1c64dc3bb3
joaoleandro <new.joaoleandro@gmail.com>
2023-10-05 19:12:01 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const ctx = document.getElementById('my-grafic');
+const mes = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho"]
+const vendedor_1 = [23, 112, 104, 57, 98, 123]
+const vendedor_2 = [47, 99, 102, 143, 139, 107]
+const vendedor_3 = [55, 44, 108, 110, 90, 101]
+
+new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: mes,
+    datasets: [{
+      label: 'Carlos',
+      data: vendedor_1,
+      borderWidth: 1
+    },{
+        label: 'Miguel',
+        data: vendedor_2,
+        borderWidth: 1
+    },{
+        label: "Lucas",
+        data: vendedor_3,
+        borderWidth: 1
+    }],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+})
